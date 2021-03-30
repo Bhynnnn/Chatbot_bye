c8e687feb2c6a800b9faf8dc06f2e3d46602da41
KangBoHyun <bhyn9785@gmail.com>
2021-03-30 19:23:12 +0900

    db

--- ChatbotApp/models.py
+++ ChatbotApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Chat(models.Model):
+    # 문자로 구성되어있는 항목
+    msg =models.CharField(max_length=100)
+    # 날짜
+    date = models.DateTimeField()
